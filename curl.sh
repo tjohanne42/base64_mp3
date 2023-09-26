71b40ae79e7d85fb0819b1c8fb9f70c5eadfdc90
Theo Johannet <theo.johannet@gmail.com>
2023-09-26 18:08:25 +0200

    update curl

--- curl.sh
+++ curl.sh
@@ -1,1 +1,2 @@
-curl -i -X POST -F filedata=@../small_base64.txt "https://base64-mp3.vercel.app/" -o "output.mp3"
+curl -i -X POST -F filedata=@../base64.txt "https://base64-mp3.onrender.com/" -o "output.mp3"
+# curl -i -X POST -F filedata=@../small_base64.txt "https://base64-mp3.vercel.app/" -o "output.mp3"
