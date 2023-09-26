237146807eb2fb0c5b200cc1ef4e5150a17d0b1d
Theo Johannet <theo.johannet@gmail.com>
2023-09-26 17:26:36 +0200

    add curl command

--- api/index.py
+++ api/index.py
@@ -6,17 +6,19 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def convert_file():
 	if request.method == 'POST':
+		print('testing')
 		try:
-			file = request.files['file'].read()
+			file = request.files['filedata'].read()
 			binary_mp3 = base64.b64decode(file)
 			response = make_response(binary_mp3)
 			response.headers.set('Content-Type', 'audio/mpeg')
+			print('done')
 			return response
-		except:
-			return f"error"
+		except Exception as e:
+			return f"error\n{str(e)}"
 	else:
 		# return jsonify({"error": str(e)})
 		return f"error"
 
-# if __name__ == '__main__':
-# 	app.run(debug=True)
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ curl.sh
@@ -0,0 +1,1 @@
+curl -i -X POST -F filedata=@base64.txt "https://base64-mp3.vercel.app/" -o "output.mp3"
